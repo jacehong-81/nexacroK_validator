fadc8e679c8222a001df525b740fde1846670b11
jacehong81 <jacehong81@tobesoft.com>
2025-04-25 13:59:53 +0900

    workspace 환경구성성

---
+++ eslint.config.mjs
@@ -0,0 +1,111 @@
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import globals from "globals";
+//import pluginJs from "@eslint/js";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["**/*.ts", "**/*.js", "**/*.mjs"], // 적용할 파일 패턴
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 2022, // ECMAScript 버전
+      sourceType: "module", // ES Module 지원, strict가 기본이므로 "no-delete-var" 같은 설정 무시가 안된다.
+      globals: {
+        ...globals.browser, // 브라우저 환경 전역 변수
+        ...globals.node, // Node.js 환경 전역 변수 등록
+        ...globals.es2021
+      },
+
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      // 기본 규칙 비활성화
+      //"function-paren-newline": "off",
+      //"no-control-regex": "off",
+      //"no-array-constructor": "off",
+      "no-self-assign": "off",
+      "no-unused-vars": "off",
+
+      "no-console": "warn",
+      /*  @ eslint-disable no-console 부터 */
+      /*       console.log(...)           */
+      /*  @ eslint-enable no-console  까지*/
+
+      // 엄격한 규칙 활성화
+      "no-delete-var": "error", // 변수 삭제 금지
+      "no-with": "error", // with 문 금지
+      "no-octal": "error", // 8진수 리터럴 금지
+      "no-octal-escape": "error", // 8진수 문자열 이스케이프 금지
+      "no-unsafe-negation": "error", // 안전하지 않은 부정 연산자 금지
+      "no-shadow-restricted-names": "error", // 예약어로 변수 이름 사용 금지 (e.g., arguments, eval)
+      "no-eval": "error", // eval() 사용 금지
+      "no-implied-eval": "error", // 암묵적인 eval 사용 금지
+      "no-iterator": "error", // __iterator__ 프로퍼티 사용 금지
+      "no-proto": "error", // __proto__ 사용 금지
+      "no-undef": "error", // 선언되지 않은 변수 금지
+      /*"no-redeclare": ["error", {
+        "ignoreDeclarationMerge": true // 중복 선언 허용- lint 버전에 따라 동작이 다름?
+      }],*/ // 동일한 변수 재선언 (var 를 썼을 경우는 무시)
+      // 코드 스타일 관련 규칙
+      "no-var": "error", // var 대신 let/const 사용 권장
+      "prefer-const": "warn", // 변경되지 않는 변수는 const 사용 권장
+      "no-use-before-define": [
+        "error",
+        {
+          functions: true, // 함수 선언 전에 호출 금지
+          classes: true, // 클래스 선언 전에 사용 금지
+          variables: true, // 변수 선언 전에 사용 금지
+        },
+      ],
+      "no-shadow": "error", //상위 스코프에서 선언된 변수 이름을 하위 스코프에서 다시 사용하는 것을 금지
+      //"prettier/prettier": "error", // Prettier 규칙 활성화
+      "brace-style": [
+        "warn",
+        "allman",
+        {
+          allowSingleLine: true,
+        },
+      ],
+      "no-redeclare": "off", // 중복 선언 허용 안 하면 "error"로
+      "@typescript-eslint/no-redeclare": "error",
+
+      "no-unused-vars": "off", // JS용 비활성화
+      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
+
+      "no-undef": "error", // JS까지 lint 대상이면 on
+
+      "no-empty-function": "off",
+      "no-useless-escape": "off",
+      "no-empty": "off",
+      "no-prototype-builtins": "off",
+
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+      //"@typescript-eslint/no-empty-object-type": "error",    // Object 타입 사용 금지 
+      //"@typescript-eslint/no-unsafe-function-type": "error", // Function 타입 사용 금지
+      //"@typescript-eslint/no-wrapper-object-types": "error", // Object, Number 등 래퍼 객체 타입 사용 금지
+      '@typescript-eslint/no-restricted-types': [ // 위 3개 아래 설정으로 변경
+        'error',
+        {
+          types: {
+            '{}': {
+              message: 'Avoid using `{}`; use a more specific type instead.',
+            },
+            Object: {
+              message: 'Avoid using `Object`; use a more specific type instead.',
+            },
+            Function: {
+              message: 'Avoid using `Function`; define a specific function type.',
+            },
+          },
+        },
+      ],
+    },
+  }
+];
---
+++ rollup-common.config.js
@@ -0,0 +1,427 @@
+import { globSync } from 'glob';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import typescript from '@rollup/plugin-typescript';
+import sourcemaps from 'rollup-plugin-sourcemaps';
+import terser from '@rollup/plugin-terser';
+import resolve from '@rollup/plugin-node-resolve';
+import json from '@rollup/plugin-json';
+import commonjs from "@rollup/plugin-commonjs";
+import replace from '@rollup/plugin-replace';
+import { visualizer } from "rollup-plugin-visualizer";
+import { execSync } from 'child_process';
+import madge from 'madge';
+import * as fs from 'fs';
+
+import { createDiagram, TsUML2Settings } from 'tsuml2'
+import { parse } from 'svg-parser'
+
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const args = process.argv.slice(2);
+const doc = args.includes('--docs') ? args[args.indexOf('--docs') + 1] : 'none';
+
+const docs_config =
+{
+    useDocument: doc == 'all' ? true : false,
+    useSVGToHtml: doc == 'all' ? true : false,
+    dir: ["src"],
+    writePath: "./docs",
+    classDiagram: "class-diagram.svg",
+    DSL: "class-diagram_d.dsl",
+    dependencyGraph: "dependency-graph.svg",
+    dependencyGraph2: "madge-dependency-graph.svg",
+    bundleAnalyzer: "bundle_analyzer.html",
+    graphOption: "full" //"short"|all""
+
+}
+
+export async function svgToHtml(config)
+{
+    const getSvgFiles = (dir) => 
+    {
+        try 
+        {
+            const files = fs.readdirSync(dir);
+            const svgFiles = files.filter(file => path.extname(file) === '.svg');
+            return svgFiles;
+        }
+        catch (error) 
+        {
+            /*  eslint-disable no-console */
+            console.error('read svg path error:', error);
+            return [];
+        }
+    };
+    const readSvgFiles = (dir, files) => 
+    {
+        return files.map(file => 
+        {
+            try 
+            {
+                const filePath = path.join(dir, file);
+                const data = fs.readFileSync(filePath, 'utf8');
+                const out = data
+                // 멤버 제거 
+                if (file == 'src-class-diagram.svg')
+                {
+                    //out = data.replace(/\[([^\|\]]+)\|[^]*?\]/g, '[$1|]');
+                    //out = out.replace(/<text[^>]*>[^<]*<\/text>/g, '');
+                }
+
+                return {
+                    fileName: file,
+                    data: out
+                };
+            }
+            catch (error) 
+            {
+                console.error(`${file} read svg file error:`, error);
+                return null;
+            }
+        }).filter(fileData => fileData !== null);
+    };
+
+    const convertToHtmlString = (node) => 
+    {
+        if (node.type === 'text') 
+        {
+            return node.value;
+        }
+
+        let attributes = '';
+        if (node.properties) 
+        {
+            attributes = Object.keys(node.properties)
+                .map(key => `${key}="${node.properties[key]}"`)
+                .join(' ');
+        }
+
+        let children = '';
+        if (node.children) 
+        {
+            children = node.children.map(child => convertToHtmlString(child)).join('');
+        }
+
+        return `<${node.tagName} ${attributes}>${children}</${node.tagName}>`;
+    };
+
+    const svgFiles = getSvgFiles(config.writePath);
+    if (svgFiles.length > 0)
+    {
+        const svgDataList = readSvgFiles(config.writePath, svgFiles);
+        svgDataList.forEach(svg => 
+        {
+            const toJsonPath = svg.fileName.replace(".svg", ".json")
+            const toHTMLPath = svg.fileName.replace(".svg", ".html")
+            try
+            {
+                if (svg.data)
+                {
+                    const processedSvgContent = svg.data.replace(/<a[^>]*>(.*?)<\/a>/g, '$1');
+
+                    const parsedSvg = parse(processedSvgContent);
+                    fs.writeFileSync(path.join(config.writePath, toJsonPath), JSON.stringify(parsedSvg, null, 4), 'utf-8');
+                    const htmlContent = convertToHtmlString(parsedSvg);
+                    const htmlString = `<!DOCTYPE html>
+                    <html lang="en">
+                    <head>
+                        <meta charset="UTF-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                        <title>${svg.fileName} - SVG to HTML Example</title>
+                    </head>
+                    <body>
+                        <h1>${svg.fileName} - SVG to HTML Example</h1>
+                        ${htmlContent}
+                    </body>
+                    </html>`;
+
+                    fs.writeFileSync(path.join(config.writePath, toHTMLPath), htmlString, 'utf-8');
+                }
+            }
+            catch (error)
+            {
+                console.error(`${svg.fileName} convert svg file error:`, error);
+            }
+
+        });
+    }
+
+
+
+
+}
+
+export async function depcruiseGraphShort(src, filepath)
+{
+    const sourceDir = "^src"
+    try 
+    {
+        execSync(`depcruise ${src} --no-config --include-only "${sourceDir}" --exclude "^dist" --ts-config ./tsconfig.json  --output-type dot | dot -T svg > ${filepath}`, { stdio: 'inherit' });
+
+        console.log('Dependency graph SVG generated successfully.');
+    }
+    catch (error) 
+    {
+        console.error('Error generating dependency graph:', error);
+    }
+}
+export async function depcruiseGraphAll(src, filepath)
+{
+    const sourceDir = "^.*"
+    try 
+    {
+        execSync(`depcruise ${src} --no-config --include-only "${sourceDir}" --exclude "^dist" --ts-config ./tsconfig.json  --output-type dot | dot -T svg > ${filepath}`, { stdio: 'inherit' });
+
+        console.log('Dependency graph SVG generated successfully.');
+    }
+    catch (error) 
+    {
+        console.error('Error generating dependency graph:', error);
+    }
+}
+
+export async function madgeGraph(src, filepath)
+{
+    const graph = await madge(src, { fileExtensions: ['ts'] })
+    const image = await graph.image(filepath)
+    console.log('madgeGraph ' + image);
+}
+export const writePackageDocumentation = async (config = docs_config) =>
+{
+    const ensureDirectoryExistence = (filePath) =>
+    {
+        const currentDirectory = process.cwd();
+        const full = path.join(currentDirectory, filePath)
+
+        if (fs.existsSync(full))
+        {
+            return true;
+        }
+
+        fs.mkdirSync(full);
+        return true
+    };
+
+
+    return {
+        name: "PackageDocumentation",
+        async writeBundle()
+        {
+            if (config.useDocument && ensureDirectoryExistence(config.writePath))
+            {
+                try
+                {
+                    for (const src of config.dir)
+                    {
+                        const cd = path.join(config.writePath, src + "-" + config.classDiagram)
+                        const dga = path.join(config.writePath, src + "-a-" + config.dependencyGraph)
+                        const dgs = path.join(config.writePath, src + "-s-" + config.dependencyGraph)
+                        const dg2 = path.join(config.writePath, src + "-" + config.dependencyGraph2)
+
+                        // add classDiagram
+                        const settings = new TsUML2Settings();
+                        settings.glob = `./${src}/**/!(*.d|*.spec|metadata_spec|type_decorators|type_class|type_metadata|method|metadata|property|class|event).ts`;
+                        settings.outFile = cd
+                        //settings.outDsl = dsl
+                        settings.exportedTypesOnly = true
+                        settings.memberAssociations = true
+                        settings.typeLinks = false
+                        settings.modifiers = false
+                        settings.propertyTypes = false
+                        settings.nomnoml = [
+
+                        ] // 적용이 안된다
+                        await createDiagram(settings);
+
+                        // add dependencyGraph
+
+                        if (config.graphOption == "all")
+                        {
+
+                            depcruiseGraphShort(src, dgs)
+                            depcruiseGraphAll(src, dga)
+
+                        }
+                        else if (config.graphOption == "short")
+                        {
+                            depcruiseGraphShort(src, dgs)
+                        }
+                        else
+                        {
+                            depcruiseGraphAll(src, dga)
+                        }
+                        madgeGraph(src, dg2)
+                    }
+
+                    if (config.useSVGToHtml)
+                    {
+                        await svgToHtml(config)
+
+                    }
+                }
+                catch (error)
+                {
+                    console.error(`Error writePackageDocumentation: ${error.message}`);
+
+                }
+            }
+
+        }
+    }
+
+
+}
+
+const getEntriesFromArray = (dir, url, arr) => Object.fromEntries(arr.map((file) => [
+    path.relative(
+        dir,
+        file.slice(0, file.length - path.extname(file).length)
+    ),
+    fileURLToPath(new URL(file, url))
+]));
+
+export function getFileMapByGlob(pattern, option, url) 
+{
+    return globSync(pattern, option).map(file => [
+        path.relative(
+            './',
+            file.slice(0, file.length - path.extname(file).length)
+        ),
+        fileURLToPath(new URL(file, url))
+    ]);
+}
+
+export function getModuleConfig(option = {}, otherRollupConfig = {}) 
+{
+    const {
+        inputFiles,
+        outputDir = './',
+        moduleRoot = './',
+        preserveModules = undefined
+    } = option;
+
+    const outputPreserveModules = preserveModules === undefined ? !isProduction : !!preserveModules;
+
+    const ret = {
+
+        input: inputFiles,
+        treeshake: true,
+        output: [
+            {
+                dir: outputDir,
+                /* if 'preserveModules' is true, it will create node_modules/tslib in dist */
+                preserveModules: outputPreserveModules,
+                preserveModulesRoot: moduleRoot,
+                format: 'es',
+                sourcemap: true,
+                minifyInternalExports: false,
+            }
+        ],
+
+        ...otherRollupConfig
+    };
+
+    return ret
+}
+
+
+export function TSBuildConfig(option = {}) 
+{
+
+    const {
+
+        sourceDir,
+        moduleUrl,
+        entries,
+        outputDir = './',
+        preserveModules = undefined,
+        pre_plugins = [],
+        post_plugins = [],
+        external = [],
+        extensions = [],
+    } = option;
+
+    const entryPoints = getEntriesFromArray(sourceDir, moduleUrl, entries.map((name) =>
+        path.join(sourceDir, name)));
+
+
+    const ret = [getModuleConfig(
+        {
+            inputFiles: entryPoints,
+            outputDir: outputDir,
+            moduleRoot: sourceDir,
+            preserveModules: preserveModules
+        },
+        {
+
+            plugins: [
+
+                ...pre_plugins,
+                replace({
+                    'preventAssignment': true,
+                    'process.env.NODE_ENV': JSON.stringify('dev')
+                }),
+
+
+                typescript({
+
+                    sourceMap: true,
+                    inlineSources: true,
+                    tsconfig: './tsconfig.json',
+                    incremental: true,
+                    exclude: ["**/*.d.ts"],  // .d.ts 파일 제외
+                    transformers:
+                    {
+                        before: [],
+                        after: [],
+                        afterDeclarations: []
+                    }
+
+                }),
+                resolve({
+                    // browser 용인지 nodejs 우선인지에 따라 아래 두개 수정
+                    browser: false, preferBuiltins: true, dedupe: ["nexacro-parser-core",
+                        "nexacro-linter",
+                        "prototype-linter",
+                        "nexacro-cli",
+                        "nexacro-pack-lagacy",
+                        "nexacro-extension-vscode",
+                        "nexacro-extension-theia"
+                    ]
+                }),
+                commonjs({
+                    extensions: [...extensions, '.js'],
+                }),
+
+
+                json({
+                    preferConst: true,
+                    compact: true
+                }),
+
+                !isProduction && sourcemaps(),
+                isProduction && terser({
+                    ecma: 2020,
+                    module: true,
+                    keep_classnames: true, // 클래스 이름 옵션
+                    keep_fnames: true,     // 함수 이름을 유지 옵션
+                }),
+
+                ...post_plugins,
+                writePackageDocumentation(docs_config),
+
+                docs_config.useDocument ? visualizer({ filename: path.join(docs_config.writePath, docs_config.bundleAnalyzer) }) : {},
+            ],
+            external: [...external,
+                'fs', 'path', 'os', 'child_process', 'url',
+                'vscode', '@theia/core' // 확장용 타겟 포함
+            ]
+        }
+    )];
+
+    return ret
+}
